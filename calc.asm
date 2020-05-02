4fd59291074afc5639b862828e76456c217f9dc3
Gabriel Nogueira <51515680+Talendar@users.noreply.github.com>
2020-05-02 19:51:47 -0300

    Add files via upload

--- calc.asm
+++ calc.asm
@@ -19,6 +19,9 @@ menu_str: .ascii "\n\n    > COOL CALCULATOR 9000 <\n"
 		  .asciiz "Operation: "
 		  
 invalid_op_str: .asciiz "Invalid operation!\n"
+invalid_op_neg: .asciiz "Invalid operand! Operand must be >= 0."
+invalid_op_neg_div: .asciiz "Invalid operand! Divisor cant be 0."
+invalid_op_neg_exp: .asciiz "Invalid operand! Expoent must be >= 0."
 result_str: .asciiz "Result: "
 print_mult: .asciiz " x "
 print_equals: .asciiz " = "
@@ -137,14 +140,8 @@ main:
 	la $ra, AFTER_OP
 	jr $s1
 	
-	# print result stored in $f12 and loop back
+	# restart menu loop
 	AFTER_OP:
-	li $v0, 4
-	la $a0, result_str
-	syscall
-	
-	li $v0, 2
-	syscall
 	j SHOW_MENU
 	
 	# exiting program
@@ -155,30 +152,62 @@ main:
 
 # Sums $f7 and $f8 and stores the result in $f12.
 sum_op:
+	addi, $sp, $sp, -4	# Alloc 4 bytes on stack
+	sw $ra, 0($sp)		# Store the return address at Stack[0]
+	
 	add.s $f12, $f7, $f8
+	
+	jal print_result
+	lw $ra, 0($sp)		# Load the return address from Stack[0]
 	jr $ra
 
 
 # Subtracts $f8 from $f7 and stores the result in $f12.
 sub_op:
+	addi, $sp, $sp, -4	# Alloc 4 bytes on stack
+	sw $ra, 0($sp)		# Store the return address at Stack[0]
+	
 	sub.s $f12, $f7, $f8
+	
+	jal print_result
+	lw $ra, 0($sp)		# Load the return address from Stack[0]
 	jr $ra
 
 
 # Multiplies $f7 and $f8 and stores the result in $f12.
 mult_op:
-	#mult $a0, $a1
-	#mflo $v0
+	addi, $sp, $sp, -4	# Alloc 4 bytes on stack
+	sw $ra, 0($sp)		# Store the return address at Stack[0]
+	
 	mul.s $f12, $f7, $f8
+	
+	jal print_result
+	lw $ra, 0($sp)		# Load the return address from Stack[0]
 	jr $ra
 
 		
-# Divides $f7 for $8 and stores the result in $f12.
+# Divides $f7 for $f8 and stores the result in $f12.
 div_op:
-	#div $a0, $a1
-	#mflo $v0
-	div.s $f12, $f7, $f8
-	jr $ra
+	# check divisor 0
+	mtc1 $zero, $f5
+	c.eq.s $f8, $f5
+	bc1f start_div
+	li $v0, 4
+	la $a0, invalid_op_neg_div
+	syscall
+	j end_div
+	
+	start_div:
+		div.s $f12, $f7, $f8
+		
+		# print result
+		addi, $sp, $sp, -4	# Alloc 4 bytes on stack
+		sw $ra, 0($sp)		# Store the return address at Stack[0]
+		
+		jal print_result
+		lw $ra, 0($sp)		# Load the return address from Stack[0]
+	end_div:
+		jr $ra
 	
 	
 # to_do: square root
@@ -188,39 +217,49 @@ sqr_op:
 	sw $ra, 0($sp)		# Store the return address at Stack[0]
 	s.s $f8, 4($sp)		# Store the radicand at Stack[1]
 	
-	# to_do: add check for negative numbers
+	# check negative input
+	mtc1 $zero, $f7
+	c.lt.s $f8, $f7
+	bc1f start_sqr
+	li $v0, 4
+	la $a0, invalid_op_neg
+	syscall
+	j sqr_after_print	
 	
 	# Initialize registers
-	mov.s $f0, $f8		# Initialize the previous aproximation register $f0
-	addi $t0, $zero, 2	# Store the integer 2 at $t0
-	mtc1 $t0, $f4		# Set $f4 to 2 using $t0
-	cvt.s.w $f4, $f4	# Convert the content of $f4 to float
-	div.s $f2, $f0, $f4	# Define an aproximation for the square root value
+	start_sqr: 
+		mov.s $f0, $f8		# Initialize the previous aproximation register $f0
+		addi $t0, $zero, 2	# Store the integer 2 at $t0
+		mtc1 $t0, $f4		# Set $f4 to 2 using $t0
+		cvt.s.w $f4, $f4	# Convert the content of $f4 to float
+		div.s $f2, $f0, $f4	# Define an aproximation for the square root value
 	
-loop_sqr:
-	# Loop this algorithm until the previous aproximation and the current one are the same
-	c.eq.s $f0, $f2
-	bc1t end_sqr
-	
-	# Calculate the offset of the aproximation by dividing the radicand by the previous aproximation
-	div.s $f6, $f8, $f0
-	
-	# Calculates the average of $f6 and $f0
-	add.s $f6, $f6, $f0	# Sum current and previous aproximations
-	div.s $f6, $f6, $f4	# Divide the result by 2, which is stored in $f4
-	
-	# Updates current and previous aproximation
-	mov.s $f0, $f2		# Update previous aproximation
-	mov.s $f2, $f6		# Update new aproximation
-	
-	j loop_sqr
+	loop_sqr:
+		# Loop this algorithm until the previous aproximation and the current one are the same
+		c.eq.s $f0, $f2
+		bc1t end_sqr
+		
+		# Calculate the offset of the aproximation by dividing the radicand by the previous aproximation
+		div.s $f6, $f8, $f0
+		
+		# Calculates the average of $f6 and $f0
+		add.s $f6, $f6, $f0	# Sum current and previous aproximations
+		div.s $f6, $f6, $f4	# Divide the result by 2, which is stored in $f4
+		
+		# Updates current and previous aproximation
+		mov.s $f0, $f2		# Update previous aproximation
+		mov.s $f2, $f6		# Update new aproximation
+		
+		j loop_sqr
 
-end_sqr:
-	# End procedure
-	mov.s $f12, $f2		# Put the result in $f12 as the return value
-	lw $ra, 0($sp)		# Load the return address from Stack[0]
-	addi $sp, $sp, 8	# Pop Stack
-	jr $ra			# Return
+	end_sqr:
+		# print result
+		mov.s $f12, $f2		# put the result in $f12 
+		jal print_result
+		sqr_after_print:
+			lw $ra, 0($sp)		# Load the return address from Stack[0]
+			addi $sp, $sp, 8	# Pop Stack
+			jr $ra			# Return
 	
 	
 # to_do: exponentiation
@@ -231,7 +270,17 @@ exp_op:
 	s.s $f7, 4($sp)		# Store the base parameter at Stack[1]
 	s.s $f8, 8($sp)		# Store the power parameter at Stack[2]
 	
+	# check negative input
+	mtc1 $zero, $f5
+	c.lt.s $f8, $f5
+	bc1f start_exp
+	li $v0, 4
+	la $a0, invalid_op_neg_exp
+	syscall
+	j exp_after_print	
+	
 	# Initialize registers
+	start_exp:
 	mov.s $f0, $f7		# Inicialize partial result register
 	addi $t1, $zero, 1	# Initialize a constant of value 1
 	
@@ -241,25 +290,28 @@ exp_op:
 	
 	# to_do: add check for negative $t0
 	
-loop_exp:
-	# Check if it reach the final result
-	beq $t0, $t1, end_exp		# End loop when power is 1
-	
-	# Calculate one interation
-	mul.s $f0, $f0, $f7		# Multiply the partial result with the base parameter
-	addi $t0, $t0, -1		# Subtracts 1 of the power for the next iteration
-	
-	# to_do: add check for overflow
-	
-	# Continue the loop
-	j loop_exp			# Redo the loop
+	loop_exp:
+		# Check if it reach the final result
+		beq $t0, $t1, end_exp		# End loop when power is 1
+		
+		# Calculate one interation
+		mul.s $f0, $f0, $f7		# Multiply the partial result with the base parameter
+		addi $t0, $t0, -1		# Subtracts 1 of the power for the next iteration
+		
+		# to_do: add check for overflow
+		
+		# Continue the loop
+		j loop_exp			# Redo the loop
 
-end_exp:
-	# End procedure
-	mov.s $f12, $f0			# Put the result in $f12 as the return value
-	lw $ra, 0($sp)			# Load the return address from Stack[0]
-	addi $sp, $sp, 12		# Pop stack
-	jr $ra				# Return
+	end_exp:
+		# print
+		mov.s $f12, $f0		# put the result in $f12 
+		jal print_result
+		# End procedure
+		exp_after_print:
+			lw $ra, 0($sp)			# Load the return address from Stack[0]
+			addi $sp, $sp, 12		# Pop stack
+			jr $ra				# Return
 	
 # to_do: multiplication table
 tab_op:
@@ -288,48 +340,45 @@ tab_op:
 	#Convert the value in $f10 to float
 	cvt.s.w $f10,$f10
 
-loop_tab:	
-	c.eq.s $f2,$f10
-	bc1t end_tab
+	loop_tab:	
+		c.eq.s $f2,$f10
+		bc1t end_tab
 	
-	add.s $f12,$f2,$f0
-	#print the number that is multiplying
-	li $v0,2
-	syscall
+		add.s $f12,$f2,$f0
+		#print the number that is multiplying
+		li $v0,2
+		syscall
 	
-	#print +
-	li $v0,4
-	la $a0,print_mult
-	syscall
+		#print +
+		li $v0,4
+		la $a0,print_mult
+		syscall
 	
-	add.s $f12,$f8,$f0
-	#print the number $f8
-	li $v0,2
-	syscall
+		add.s $f12,$f8,$f0
+		#print the number $f8
+		li $v0,2
+		syscall
 	
-	#print =
-	li $v0,4
-	la $a0,print_equals
-	syscall
+		#print =
+		li $v0,4
+		la $a0,print_equals
+		syscall
 	
-	mul.s $f12,$f2,$f8
+		mul.s $f12,$f2,$f8
 	
-	#print the result
-	li $v0,2
-	syscall
+		#print the result
+		li $v0,2
+		syscall
+		jal print_new_line
 	
-	jal print_new_line
-	
-	#increment $f1
-	add.s $f2,$f2,$f1
-	
-	j loop_tab
+		#increment $f1
+		add.s $f2,$f2,$f1
+		j loop_tab
 
-end_tab:
-	jal print_new_line
-	lw $ra, 0($sp)
-	addi $sp, $sp, 4
-	jr $ra
+		end_tab:
+			lw $ra, 0($sp)
+			addi $sp, $sp, 4
+			jr $ra
 
 		
 # to_do: body mass index
@@ -340,15 +389,31 @@ bmi_op:
 	s.s $f7, 4($sp)		# Store the weight parameter at Stack[1]
 	s.s $f8, 8($sp)		# Store the height parameter at Stack[2]
 	
-	# Calculates body mass (weight / (height * height))
-	mul.s $f1, $f8, $f8	# Multiply height by itself and store at $f1
-	div.s $f0, $f7, $f1	# Divide weight by height squared and store at $f0
+	# check negative input
+	mtc1 $zero, $f5
+	c.lt.s $f7, $f5
+	bc1t bmi_print_error
+	c.lt.s $f8, $f5
+	bc1f bmi_start
+	bmi_print_error:
+		li $v0, 4
+		la $a0, invalid_op_neg
+		syscall
+		j bmi_after_print
 	
+	# Calculates body mass (weight / (height * height))
+	bmi_start:
+		mul.s $f1, $f8, $f8	# Multiply height by itself and store at $f1
+		div.s $f0, $f7, $f1	# Divide weight by height squared and store at $f0
+	
+		# Print
+		mov.s $f12, $f0		# put the result in $f12 
+		jal print_result
 	# End procedure
-	mov.s $f12, $f0		# Put the result in $f12 as the return value
-	lw $ra, 0($sp)		# Load the return address from Stack[0]
-	addi $sp, $sp, 12	# Pop stack
-	jr $ra			# Return
+	bmi_after_print:
+		lw $ra, 0($sp)		# Load the return address from Stack[0]
+		addi $sp, $sp, 12	# Pop stack
+		jr $ra			# Return
 		
 #Calculates the integer part of $f8 factorial
 #Parameter: $f8 will be truncate, so just the integer part will be use
@@ -359,6 +424,16 @@ fact_op:
 	sw $ra, 0($sp)		# Store the return address at Stack[0]
 	s.s $f8, 4($sp)		# Store the parameter at Stack[1]
 	
+	# check negative input
+	mtc1 $zero, $f5
+	c.lt.s $f8, $f5
+	bc1f start_fact
+	li $v0, 4
+	la $a0, invalid_op_neg
+	syscall
+	j fact_after_print	
+	
+	start_fact:
 	#t0 will store the integer part of the $f8
 	cvt.w.s $f8,$f8  #Convert the value of $f8 from single to integer
 	mfc1 $t0,$f8     #Put the integer value of $f8 in $t0
@@ -387,7 +462,6 @@ fact_op:
 		bc1t end_fact
 		
 		add.s $f2,$f2,$f1	#Increase the $f2
-		
 		mul.s $f12,$f12,$f2	#Multiple $F12 by the $f2
 		
 		#Go back to the loop
@@ -395,17 +469,31 @@ fact_op:
 	
 	# End procedure
 	end_fact:
+		# print result
+		jal print_result
 		
-		lw $ra, 0($sp)		# Load the return address from Stack[0]
-		addi $sp, $sp, 8	# Pop stack
-		jr $ra			# Return
-		
-		jr $ra
+		fact_after_print:
+			lw $ra, 0($sp)		# Load the return address from Stack[0]
+			addi $sp, $sp, 8	# Pop stack
+			jr $ra			# Return
 
 		
 # Prints all the items of the Fibonacci sequence up to the item with index equal to the value stored in $f8. Returns 0 ($f12 = 0).
 fib_op:
+	addi $sp, $sp, -4	# Alloc 4 bytes on stack
+	sw $ra, 0($sp)		# Store the return address at Stack[0]
+
+	# check negative input
+	mtc1 $zero, $f5
+	c.lt.s $f8, $f5
+	bc1f FIB_NO_ERROR
+	li $v0, 4
+	la $a0, invalid_op_neg
+	syscall
+	j END_FIB	
+
 	# initializing registers
+	FIB_NO_ERROR:
 	li $t0, 2  # iteration counter (starts with the 3rd element of the sequence)
 	li $t1, 0  # element of index n-2
 	li $t2, 1  # element of index n-1
@@ -447,9 +535,9 @@ fib_op:
 		beq $t7, 1, FIB_START
 	
 	END_FIB: 
-	jal print_new_line
-	l.s $f12, fp_zero
-	jr $t6
+		lw $ra, 0($sp)		# Load the return address from Stack[0]
+		addi $sp, $sp, 4	# Pop stack
+		jr $t6
 	
 	
 # Prints a new line.
@@ -464,5 +552,16 @@ print_new_line:
 print_space:
 	li $a0, 32
 	li $v0, 11
+	syscall
+	jr $ra
+	
+
+# Prints the result stored in the register $f12.
+print_result:
+	li $v0, 4
+	la $a0, result_str
+	syscall
+	
+	li $v0, 2
 	syscall
 	jr $ra
