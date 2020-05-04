bb6413ac278e9e5e73addc2f7a6ef77310be212d
Gabriel Nogueira <51515680+Talendar@users.noreply.github.com>
2020-05-04 12:04:10 -0300

    Update bubble_sort.asm

--- sorting_algorithms/bubble_sort.asm
+++ sorting_algorithms/bubble_sort.asm
@@ -8,7 +8,7 @@ new_line_char: .asciiz "\n"
 space_char: .asciiz " "
 	.align 2
 array: .word 5, 2, 10, 3, 14, 8, 4, 0, 9, 1, 6, 7, 15, 11, 12, 13   # array to be sorted
-n: .word 16  								# size of the array
+n: .word 16  							    # size of the array
 
 
 	.text
@@ -21,7 +21,7 @@ main:
 	la $a0, array  		# loading the array's address into $a0
 	la $a1, n
 	lw $a1, 0($a1) 		# loading the array size n into $a1
-	jal bubble_sort     # sorting	
+	jal bubble_sort     	# sorting	
 	
 	jal print_new_line
 	
@@ -52,14 +52,14 @@ bs_swap_loop:							# loops until no swap was performed
 	li $t1, 0							# i ($t1) = 0
 
 bs_for_loop:							# for(int i = 1; i < n; i++)
-	bge $t1, $a1, bs_for_end			# break if i >= n
+	bge $t1, $a1, bs_for_end				# break if i >= n
 	
 	sll $t3, $t1, 2						# multiplying i by 4 and storing the result in $t3
 	add $t3, $t3, $a0					# getting the address of array[i] and putting it in $t3
 	lw $t6, 0($t3)						# a ($t6) = array[i]
 	lw $t7, 4($t3)						# b ($t7) = array[i+1]
 	
-	ble $t6, $t7, bs_for_inc			# if(a > b)
+	ble $t6, $t7, bs_for_inc				# if(a > b)
 	sw $t6, 4($t3)						# array[i] = array[i+1]
 	sw $t7, 0($t3)						# array[i+1] = array[i] (previous value)
 	li $t0, 1							# swapped = true
@@ -69,7 +69,7 @@ bs_for_inc:
 	j bs_for_loop			
 	
 bs_for_end:
-	bne $t0, $zero, bs_swap_loop		# restart swap loop if swapped = true
+	bne $t0, $zero, bs_swap_loop				# restart swap loop if swapped = true
 	
 	lw $ra, 0($sp)  					# recovering the return address from the stack
 	addi $sp, $sp, 4
@@ -93,7 +93,7 @@ print_array:
 	move $s0, $a0						# uses $s0 to hold the array's address
 	li $s1, 0							# i ($s1) = 0
 print_array_start:						# for(i = 0; i < n; i++)
-	bge $s1, $a1, print_array_end		# break if i >= n
+	bge $s1, $a1, print_array_end				# break if i >= n
 	
 	sll $s2, $s1, 2						# multiplying i by 4 and storing the result in $s2
 	add $s2, $s2, $s0					# getting the address of array[i] and putting it in $s2				
